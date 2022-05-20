da04b0d4ab25de6b6866517b5ef9f86345195c43
ra54283suzuki <ra54283suzuki@gmail.com>
2022-05-20 23:26:22 +0900

    HTTP Method Discrimination

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const http = require("http");
 const { runInNewContext } = require("vm");
 
 http.createServer(function (req, res) {
-  if(req.url === "/") {
+  if(req.url === "/" && req.method === "GET") {
     res.setHeader("Content-Type", "text/plain;charset=utf-8");
     res.write("こんにちは");
     res.end();
-  } else if(req.url === "/about") {
+  } else if(req.url === "/about" && req.method === "GET") {
     res.setHeader("Content-Type", "text/plain;charset=utf-8");
     res.write("ここはAboutページです");
     res.end();
+  } else if(req.url === "/hobby" && req.method === "GET") {
+    res.setHeader("Content-Type", "text/html");
+    res.write('<form action="/outdoor" method="POST"><input type="text" name="sports"><button type="submit">Submit</button></form>');
+    res.end();
+  } else if(req.url === "/outdoor" && req.method === "POST") {
+    console.log("reqの中身:", req);
   }
 }).listen(4000, function () {
   console.log("Listening on localhost port 4000");
